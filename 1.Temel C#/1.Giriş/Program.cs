d5a1b3781fde749bfafae0d4c56ae882f2591ac2
Emre Aydın <Eaydn81@outlook.com>
2022-08-11 11:52:23 +0300

    Add files via upload

---
+++ 1.Temel C#/1.Giriş/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Giriş
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba Dünya!");
+            Console.ReadLine();
+        }
+    }
+}
