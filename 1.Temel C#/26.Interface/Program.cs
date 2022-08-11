d58b4069cd4e7ecd8b2f9f5f92375ab1f743d52a
Emre Aydın <Eaydn81@outlook.com>
2022-08-11 12:06:15 +0300

    Add files via upload

---
+++ 1.Temel C#/26.Interface/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _25.Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ 1.Temel C#/26.Interface/Truck.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25.Interface
+{
+    internal class Truck : IVehicle
+    {
+        public void Brake()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Gaslevel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move(int speed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
